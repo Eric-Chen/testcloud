7dd0ecbfcf7761825af62502c4d70d54152e7660
ericchen <ericchen1984@126.com>
2017-04-11 21:05:19 +0800

    updated

---
+++ cloudconfig/src/main/java/com/testcloud/cloudconfig/ConfigServer.java
@@ -0,0 +1,10 @@
+package com.testcloud.cloudconfig;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by eric on 11/04/2017.
+ */
+@SpringBootApplication
+public class ConfigServer {
+}
