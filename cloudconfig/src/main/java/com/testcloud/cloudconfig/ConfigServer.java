2f40fb96391776a7dbf4dff438763b401e2b919c
ericchen <ericchen1984@126.com>
2017-04-11 21:07:13 +0800

    updated

--- cloudconfig/src/main/java/com/testcloud/cloudconfig/ConfigServer.java
+++ cloudconfig/src/main/java/com/testcloud/cloudconfig/ConfigServer.java
@@ -1,10 +1,18 @@
 package com.testcloud.cloudconfig;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
  * Created by eric on 11/04/2017.
  */
 @SpringBootApplication
+@EnableConfigServer
 public class ConfigServer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServer.class, args);
+    }
+
 }
