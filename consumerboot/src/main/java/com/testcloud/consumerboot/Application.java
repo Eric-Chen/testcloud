5b0350d73a697eaa7349c917c1cc2a930ac7cff4
ericchen <ericchen1984@126.com>
2017-04-13 19:16:39 +0800

    updated

--- consumerboot/src/main/java/com/testcloud/consumerboot/Application.java
+++ consumerboot/src/main/java/com/testcloud/consumerboot/Application.java
@@ -1,10 +1,16 @@
 package com.testcloud.consumerboot;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * Created by eric on 13/04/2017.
  */
+@SpringBootApplication
+@ComponentScan
+@EnableEurekaClient
 public class Application {
 
     public static void main(String[] args) {
--- testboot/src/main/java/com/testcloud/testboot/Main.java
+++ testboot/src/main/java/com/testcloud/testboot/Main.java
@@ -2,7 +2,7 @@ package com.testcloud.testboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.ComponentScan;
  */
 @SpringBootApplication
 @ComponentScan
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class Main {
 
     public static void main(String[] args) {
